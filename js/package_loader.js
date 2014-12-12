c74852b55e1eae12867ab93a3d2975a740b37306
kitatemus <daniel.j.jurman@gmail.com>
2014-12-12 17:08:34 -0500

    mannn

--- js/package_loader.js
+++ js/package_loader.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
   var folders = [ "dead-witch",
                   "dogtown",
                   "dead-witch2",
-                  "dogtown2"];
+                  "dogtown2" ];
 
   // add the initial row (where the images will sit)
   $("#thumbnails").last().append('<div class="thumbs row packaging">');
