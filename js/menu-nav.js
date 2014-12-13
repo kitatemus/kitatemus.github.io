846ab0888e892a0bb5e925a4f7feb94f459fe2ed
kitatemus <daniel.j.jurman@gmail.com>
2014-12-12 22:32:06 -0500

    123goooooo

--- js/menu-nav.js
+++ js/menu-nav.js
@@ -1,4 +1,11 @@
 // when the document has been loaded
 $(document).ready(function() {
   $("#submit-form").hide();
+
+  $("#about-me-button").click( function() {
+    $("#thumbnails").hide();
+    $("#fulls").hide();
+
+    $("#submit-form").show();
+  });
 });
