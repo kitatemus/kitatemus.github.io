76bdcda07b8c196c070b6c7fa650d81739e6487c
Jesse Jurman <j.r.jurman@gmail.com>
2015-01-08 19:26:27 -0500

    hide / show function

--- js/main-page.js
+++ js/main-page.js
@@ -1,24 +1,38 @@
+// define back button early
 back_button = function(){};
+
+/* hide or show a set of elements
+ * param: selector can be any selector
+ * param: display should be either 'none' (to hide) or '' (to show)
+ */
+set_display_selector = function(selector, display) {
+  ele = document.querySelectorAll(selector);
+  Array.prototype.forEach.call(ele, function(e) {
+    ele.style.display = display;
+  });
+}
 
 // when a navigation link has been clicked
 function nav_content_switcher(type) {
   if (type == 'about-me') {
-    document.querySelectorAll(".thumbs.row").style.display = 'none';
-    document.querySelectorAll(".full-images").style.display = 'none';
+    set_display_selector(".thumbs.row", 'none');
+    set_display_selector(".full-images", 'none');
 
-    document.querySelectorAll("#about-me").style.display = '';
+    set_display_selector("#about-me", '');
   }
   else {
-    document.querySelectorAll("#about-me").style.display = 'none';
-    document.querySelectorAll(".thumbs.row").style.display = 'none';
-    document.querySelectorAll(".full-images").style.display = 'none';
+    set_display_selector("#about-me", 'none');
+    set_display_selector(".thumbs.row", 'none');
+    set_display_selector(".full-images", 'none');
 
-    document.querySelectorAll( ".thumbs.row."+type ).style.display = '';
+    set_display_selector( ".thumbs.row."+type , '');
 
     // when a back button has been clicked
     back_button = function() {
-      document.querySelectorAll("#about-me").style.display = 'none';
-      document.querySelectorAll( ".thumbs.row."+type ).style.display = '';
+      set_display_selector("#about-me", 'none');
+      set_display_selector(".full-images", 'none');
+
+      set_display_selector( ".thumbs.row."+type , '');
     }
 
   }
@@ -27,7 +41,7 @@ function nav_content_switcher(type) {
 // when the document has been loaded
 document.addEventListener('DOMContentLoaded', function(){
 
-  document.querySelectorAll(".thumbs.row.posters").style.display = 'none';
-  document.querySelectorAll("#about-me").style.display = 'none';
+  set_display_selector(".thumbs.row.posters", 'none');
+  set_display_selector("#about-me", 'none');
 
 });
