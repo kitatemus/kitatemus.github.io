8d61fb5e08281f75198ba6f04d87bb461536ed6b
Jesse Jurman <j.r.jurman@gmail.com>
2015-01-08 21:42:40 -0500

    trying new loader, errors are very possible

--- js/package_loader.js
+++ js/package_loader.js
@@ -1,3 +1,19 @@
+// image object that sets up folders
+var imagesObj = {
+  'packaging': ["wegmans",
+                "owl-house",
+                "forge",
+                "dogtown",
+                "dead-witch" ],
+
+  'posters': ["america",
+              "japan",
+              "children",
+              "black-metal",
+              "detroit"]
+}
+
+// check if a url exists
 function UrlExists(url)
 {
     var http = new XMLHttpRequest();
@@ -6,29 +22,31 @@ function UrlExists(url)
     return http.status!=404;
 }
 
+// function to append element to last of a selection
+function append_last(selector, element) {
+  var selection = document.querySelectorAll(selector);
+  var last_selection = selection[selection.length - 1];
+  last_selection.appendChild(element);
+}
 
-// when the document has been loaded
-$(document).ready(function() {
-
-  // for every packaging folder
-  var folders = [ "wegmans",
-                  "owl-house",
-                  "forge",
-                  "dogtown",
-                  "dead-witch"];
+/* function to load images
+ * root can be packaging or posters... 'images-' will be prepended when appr.
+ * folders should be a list with each folder name inside 'images-<root>'
+ */
+function folder_loaders(root, folders) {
 
   // add the initial row (where the images will sit)
-  $("#thumbnails").last().append('<div class="thumbs row packaging">');
+  append_last("#thumbnails", '<div class="thumbs row '+ root +'">');
 
   // iterate through each packaging folder
-  $.each( folders, function(index,folder) {
+  Array.prototype.forEach.call( folders, function(folder, index) {
 
-    // get the package info
-    thumb_path = "images-packaging/" + folder + "/thumb.png";
-    alt_text_path = "images-packaging/" + folder + "/alt_text.txt";
+    // get the image info
+    var thumb_path = "images-" + root + "/" + folder + "/thumb.png";
+    var alt_text_path = "images-" + root + "/" + folder + "/alt_text.txt";
 
-    // append a div for the thumbnail and text
-    $(".thumbs.row").last().append('\
+    // div with thumb and text
+    var new_element = '\
       <div id="'+folder+'-thumb" class="three columns">\
         \
         <a href="#">\
@@ -36,23 +54,35 @@ $(document).ready(function() {
         </a>\
         <p class="'+folder+'-thumb alt-text"></p>\
       </div>\
-    ');
+    ';
 
-    $("p."+folder+"-thumb.alt-text").load(alt_text_path);
+    // append the div
+    append_last(".thumbs.row", new_element)
+
+    // load in the alt_text
+    var alt_text_loader = new XMLHttpRequest();
+    alt_text_loader.open("GET",alt_text_path,true);
+    alt_text_loader.send();
+
+    alt_text_loader.onreadystatechange = function() {
+      if (this.readyState== 4 && this.status == 200){
+        alt_element = document.querySelector("p."+folder+"-thumb.alt-text");
+        alt_element.innerHTML = this.responseText;
+      }
+    }
 
     // if this is the last image in a row
     if (index%3 == 2) {
       // start the next row
-      $("#thumbnails").last().append('<div class="thumbs row packaging">');
+      append_last("#thumbnails", '<div class="thumbs row ' + root + '">')
     }
 
     // get the info for the full page version
-    full_path = "images-packaging/" + folder + "/full.png";
-    full_text_path = "images-packaging/" + folder + "/full_text.txt";
-
+    var full_path = "images-" + root + "/" + folder + "/full.png";
+    var full_text_path = "images-" + root + "/" + folder + "/full_text.txt";
 
     // append a div for the image and text
-    $("#fulls").append('\
+    new_element = '\
       <div id="'+folder+'-full" class="full-images row">\
         <div class="'+folder+'-full image-container eight columns">\
           <img class="full-image" src=' + full_path + ' width="100%">\
@@ -61,31 +91,54 @@ $(document).ready(function() {
           <p class="'+folder+'-full full-text"></p>\
         </div>\
       </div>\
-    ')
+    ';
+
+    // append the div
+    append_last("#fulls", new_element)
 
     // try to pull in other images (e.g. full1.png, full2.png, etc...)
-    full_path_part = "images-packaging/" + folder + "/full";
-    current_img = 1;
-    current_path = full_path_part + current_img + ".png";
+    var full_path_part = "images-" + root + "/" + folder + "/full";
+    var current_img = 1;
+    var current_path = full_path_part + current_img + ".png";
+
     // while we still have images to load
     while ( UrlExists(current_path) ) {
 
       // append those images to the div
-      $("."+folder+"-full.image-container").append('\
-        <img class="full-image" src=' + current_path + ' width="100%">\
-      ')
+      var new_full = '<img class="full-image" src='+ current_path +' width="100%">';
+
+      append_last("."+folder+"-full.image-container", new_full);
 
       // bump up the image number
       current_img = current_img + 1;
       current_path = full_path_part + current_img + ".png";
     }
 
-    $("p."+folder+"-full.full-text").load(full_text_path);
-    $("."+folder+"-full.image-container").append('\
+    // load in the full_text
+    var full_text_loader = new XMLHttpRequest();
+    full_text_loader.open("GET",alt_text_path,true);
+    full_text_loader.send();
+
+    alt_text_loader.onreadystatechange = function() {
+      if (this.readyState== 4 && this.status == 200){
+        full_element = document.querySelector("p."+folder+"-full.full-text");
+        full_element.innerHTML = this.responseText;
+      }
+    }
+
+    // append back button
+    var back_button = '\
       <a class="packaging-back-button" href="#" onclick="back_button()">back</a>\
-    ');
+    ';
+    append_last("."+folder+"-full.image-container", back_button);
 
   });
+}
+
+// when the document has been loaded
+$(document).ready(function() {
+
+  folder_loaders("packaging", imagesObj["packaging"]);
 
   $(".full-images").hide();
 
