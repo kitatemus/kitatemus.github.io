803e9d3024027083bb96e834768ea5f24b5653aa
Jesse Jurman <j.r.jurman@gmail.com>
2015-01-13 00:27:58 -0500

    probably broken

--- js/image_loader.js
+++ js/image_loader.js
@@ -100,25 +100,48 @@ function folder_loaders(root, folders) {
       append_last("#thumbnails", next_row)
     }
 
+    // load in the full_text
+    var full_text;
+    var full_text_loader = new XMLHttpRequest();
+    full_text_loader.open("GET",full_text_path,true);
+    full_text_loader.send();
+
+    full_text_loader.onreadystatechange = function() {
+      if (this.readyState== 4 && this.status == 200){
+        full_text = this.responseText;
+      }
+    }
+
+    // prepare full text for multiple images
+    var text_blocks = full_text.split(";");
+
     // get the info for the full page version
     var full_path = "images-" + root + "/" + folder + "/full.png";
     var full_text_path = "images-" + root + "/" + folder + "/full_text.txt";
 
-    // div with thumb and text
-    var new_element = document.createElement('div');
-    new_element.setAttribute('class', "full-images row");
-    new_element.setAttribute('id', folder+'-full');
-    new_element.innerHTML = '\
-      <div class="'+folder+'-full image-container eight columns">\
-        <img class="full-image" src=' + full_path + ' width="100%">\
-      </div>\
-      <div class="'+folder+'-full text-container four columns">\
-        <p class="'+folder+'-full full-text"></p>\
-      </div>\
-    ';
+    var full_images_div = document.createElement('div');
+    full_images_div.setAttribute('class', "full-images row");
+    full_images_div.setAttribute('id', folder+'-full');
 
-    // append the div
-    append_last("#fulls", new_element)
+    function newFullSection(folder, imageSrc, text) {
+      var new_image_div = document.createElement('div');
+      new_image_div.setAttribute('class', folder+"-full image-container eight columns");
+      new_image_div.innerHTML = '\
+        <img class="full-image" src=' + full_path + ' width="100%">\
+      ';
+
+      append_last('#'+folder+'-full', new_image_div);
+
+      var new_text_div = document.createElement('div');
+      new_text_div.setAttribute('class', folder+"-full text-container four columns");
+      new_text_div.innerHTML = '\
+        <p class="'+folder+'-full full-text">'+text+'</p>\
+      ';
+
+      append_last('#'+folder+'-full', new_text_div);
+    }
+
+    newFullSection(folder, full_path, text_blocks[0]);
 
     // try to pull in other images (e.g. full1.png, full2.png, etc...)
     var full_path_part = "images-" + root + "/" + folder + "/full";
@@ -127,31 +150,20 @@ function folder_loaders(root, folders) {
 
     // while we still have images to load
     while ( UrlExists(current_path) ) {
-
-      // append those images to the div
-      var new_full = document.createElement("img");
-      new_full.setAttribute("class", "full-image");
-      new_full.setAttribute("src", current_path);
-      new_full.setAttribute("width", "100%");
-
-      append_last("."+folder+"-full.image-container", new_full);
+      // append those images (and text) to the div
+      var new_text = "";
+      if (text_blocks[current_img]) {
+        new_text = text_blocks[current_img];
+      }
+      newFullSection(folder, current_path, new_text);
 
       // bump up the image number
       current_img = current_img + 1;
       current_path = full_path_part + current_img + ".png";
     }
 
-    // load in the full_text
-    var full_text_loader = new XMLHttpRequest();
-    full_text_loader.open("GET",full_text_path,true);
-    full_text_loader.send();
-
-    full_text_loader.onreadystatechange = function() {
-      if (this.readyState== 4 && this.status == 200){
-        full_element = document.querySelector("p."+folder+"-full.full-text");
-        full_element.innerHTML = this.responseText;
-      }
-    }
+    // append the div
+    append_last("#fulls", full_images_div);
 
     // append back button
     var back_button = document.createElement('a');
